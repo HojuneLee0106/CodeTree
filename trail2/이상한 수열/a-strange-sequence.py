e1bd53257b4839ef29ad63bfde22b3007450a71c
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-05-27 16:22:37 +0900

    [Passed] 이상한 수열 (48ms, 16MB)

---
+++ trail2/이상한 수열/a-strange-sequence.py
@@ -0,0 +1,10 @@
+N = int(input())
+def cal(x):
+    if x==1:
+        return 1
+    elif x==2:
+        return 2
+    else:
+        return cal(x//3)+cal(x-1)
+print(cal(N))
+# Please write your code here.
