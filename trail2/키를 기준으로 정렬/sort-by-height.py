cfd7f6b2ed2b2faaba0420fbe3e26ae517548e37
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-06-01 17:09:55 +0900

    [Passed] 키를 기준으로 정렬 (49ms, 16MB)

---
+++ trail2/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,23 @@
+n = int(input())
+name = []
+height = []
+weight = []
+
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+class profile:
+    def __init__(self,name, height, weight):
+        self.name=name
+        self.height=height
+        self.weight=weight
+pro=[]
+for i in range(n):
+    x=profile(name[i], height[i],weight[i])
+    pro.append(x)
+pro.sort(key=lambda x:x.height)
+for i in range(n):
+    print(pro[i].name, pro[i].height, pro[i].weight)
+# Please write your code here.
