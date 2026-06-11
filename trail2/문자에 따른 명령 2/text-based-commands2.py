1fa26cfd9acde5e38f38cfd3d3148307c3f740ee
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-06-11 16:12:50 +0900

    [Passed] 문자에 따른 명령 2 (68ms, 17MB)

---
+++ trail2/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,23 @@
+dirs = input()
+dir=["N","E","S","W"]
+curr=0
+x=0
+y=0
+for i in range(len(dirs)):
+    if dirs[i]=="F":
+        if curr==0:
+            y+=1
+        elif curr==1:
+            x+=1
+        elif curr==2:
+            y-=1
+        else:
+            x-=1
+    elif dirs[i]=="L":
+        curr-=1
+        curr%=4
+    else:
+        curr+=1
+        curr%=4
+print(x,y)
+# Please write your code here.
