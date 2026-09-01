d260fc6692b1a3e3d6dadc2cf638b57a9ed00fde
HojuneLee <147648712+HojuneLee0106@users.noreply.github.com>
2026-09-01 17:49:51 +0900

    [Passed] 정수 명령 처리 5 (123ms, 21MB)

---
+++ trail3/정수 명령 처리 5/process-numeric-commands-5.py
@@ -0,0 +1,23 @@
+N = int(input())
+
+command = []
+num = []
+
+for _ in range(N):
+    line = input().split()
+    command.append(line[0])
+    if line[0] == "push_back" or line[0] == "get":
+        num.append(int(line[1]))
+    else:
+        num.append(0)
+answer=[]
+for i in range(N):
+    if command[i] == "push_back":
+        answer.append(num[i]) 
+    elif command[i] == "get":
+        print(answer[num[i]-1])
+    elif command[i] == "size":
+        print(len(answer))
+    else:
+        answer.pop()
+# Please write your code here.
